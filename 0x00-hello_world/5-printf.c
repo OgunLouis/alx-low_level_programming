e9734f24e6622f5f9e52264fbead8a78bd1cb71c
Ogunnowo <ogunnowolouis@gmail.com>
2023-02-16 09:47:07 -0800

    print

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 
 /**
- * main - Entry point
- * Description: prints exactly with proper grammar, but the outcome is a piece of art,
- * Return: Always 0 (Success)
+ * main - prints "prints exactly with proper grammar, but the outcome is 
+ * a piece of art,"
+ * Return: Always 0 
  */
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
