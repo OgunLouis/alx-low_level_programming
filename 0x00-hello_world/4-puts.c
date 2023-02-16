eb65706c6a562158f9bdcfe3eda3bf9e7232073a
Ogunnowo <ogunnowolouis@gmail.com>
2023-02-16 08:26:42 -0800

    k

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 
 /**
- * *main prints "Programming is like building a multilingual puzzle
+ * *main - prints "Programming is like building a multilingual puzzle
  * followed by a new line
  * Return always 0
  */
 
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle\n");
+	puts("\"Programming is like building a multilingual puzzle");
 	return(0);
 }
