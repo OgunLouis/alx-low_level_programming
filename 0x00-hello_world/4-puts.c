d7cca104d994f172729ac443bfe9af3ca4a75798
Ogunnowo <ogunnowolouis@gmail.com>
2023-02-16 09:25:48 -0800

    k

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,9 @@
 #include <stdio.h>
 
 /**
- * main - Entry point
  * main - prints "Programming is like building a multilingual puzzle
  * followed by a new line
- * Return Ailways 0
+ * Return: Always 0
  */
 
 int main(void)
