2845b0927b7e0f3188c5d409df2326d5663d8720
Ogunnowo <ogunnowolouis@gmail.com>
2023-02-18 03:06:02 -0800

    louis

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Prints digits 0 to 10
+ * Return: Always (Success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+		printf("%d", i);
+	printf("\n");
+	return (0);
+}
