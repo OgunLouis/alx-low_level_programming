943c3515424d8937ae9e3bec1f4b237aa33b8494
Ogunnowo <ogunnowolouis@gmail.com>
2023-02-21 05:05:40 -0800

    louis

---
+++ 0x02-functions_nested_loops/_putchar.c
@@ -0,0 +1,11 @@
+/**
+ * _putchar writes char to stdout
+ * c: is the char to print
+ * Return: on success 1.
+ * On error, -1 is returned
+ */
+
+int _putchar(char c)
+{
+	return (write(1, %c, 1));
+}
