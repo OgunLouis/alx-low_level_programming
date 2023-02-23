a1e773e107642cc23dd40f2b9811464fa3758bde
Ogunnowo <ogunnowolouis@gmail.com>
2023-02-23 06:30:59 -0800

    louis

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_numbers - check
+ * Return: 1 for upper letter and 0 for lower
+ */
+
+void print_numbers(void)
+{
+	int x;
+
+	for (x = 0; x <= 9; x++)
+	{
+		_putchar(x + 0);
+	}
+	_putchar('\n');
+}
