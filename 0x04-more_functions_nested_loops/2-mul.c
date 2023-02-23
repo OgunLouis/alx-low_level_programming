020ce3a6f6ed995cf1354918d1cff9112745d605
Ogunnowo <ogunnowolouis@gmail.com>
2023-02-23 06:21:31 -0800

    louis

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * mul - check
+ * @a: the number
+ * @b: the number
+ * Return: 1 for upper letter and 0 for lower
+ */
+
+int mul(int a, int b)
+
+{
+	return (a * b);
+}
