0783e49f1f8d51657f6b3a3a6c36115385f2ea59
Ogunnowo <ogunnowolouis@gmail.com>
2023-02-23 15:10:45 -0800

    louis

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -1,16 +1,16 @@
 #include "main.h"
 
 /**
- * print_diagonal - print
- * @n: - func
- * Return: empty
- */
+* print_diagonal - print
+* @n: - func
+* Return: empty
+*/
 
 void print_diagonal(int n)
 
 {
 	int x, y;
-	
+
 	if (n <= 0)
 	{
 		_putchar('\n');
