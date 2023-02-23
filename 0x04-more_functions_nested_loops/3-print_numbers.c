9ad5f81c72c5c1bba67dc1e8a24a2fc70707f55c
Ogunnowo <ogunnowolouis@gmail.com>
2023-02-23 06:41:19 -0800

    louis

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -7,11 +7,12 @@
 
 void print_numbers(void)
 {
-	int x;
+	int c;
+	int _putchar(char c);
 
-	for (x = 0; x <= 9; x++)
+	for (c = 0; c <= 9; c++)
 	{
-		_putchar(x + 0);
+		_putchar(c + 0);
 	}
 	_putchar('\n');
 }
