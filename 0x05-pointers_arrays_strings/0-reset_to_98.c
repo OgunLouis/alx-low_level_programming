bc2d76b5e9527bd6505875a547780144224d28b0
Ogunnowo <ogunnowolouis@gmail.com>
2023-02-27 08:35:25 -0800

    louis

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 #include "main.h"
 /*
- * reset_to_98 - reset
- * @n: to be reset
+ * reset_to_98 - resets the value to return 98
+ * @n:value to be reset
  * Return: nothing
  */
 void reset_to_98(int *n)
