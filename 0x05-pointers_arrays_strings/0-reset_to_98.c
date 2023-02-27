508345a9a7c5b910eff7dc78bda48c379bf73385
Ogunnowo <ogunnowolouis@gmail.com>
2023-02-27 08:54:09 -0800

    louis

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -3,12 +3,12 @@
 /*
  * reset_to_98 - Takes a pointer to int and updates value to 98
  *
- * @n: int input pointet n
+ * @*n: int input pointet n
  *
  * Return : nothing
  */
 
-void reset_to_98 (int *n)
+void reset_to_98(int *n)
 
 {
 	*n = 98;
